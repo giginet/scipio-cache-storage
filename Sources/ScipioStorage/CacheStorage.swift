3b286ae2130b3aefb2efa8bf059065e5dc613350
giginet <giginet.net@gmail.com>
2024-05-16 22:06:25 +0900

    Add extensions

--- Sources/ScipioStorage/CacheStorage.swift
+++ Sources/ScipioStorage/CacheStorage.swift
@@ -10,3 +10,25 @@ public protocol CacheStorage {
 public protocol CacheKey: Hashable, Codable, Equatable {
     var targetName: String { get }
 }
+
+extension CacheKey {
+    public var frameworkName: String {
+        "\(targetName.packageNamed()).xcframework"
+    }
+}
+
+extension CacheStorage {
+    public var parallelNumber: Int? {
+        nil
+    }
+}
+
+extension String {
+    func packageNamed() -> String {
+        // Xcode replaces any non-alphanumeric characters in the target with an underscore
+        // https://developer.apple.com/documentation/swift/imported_c_and_objective-c_apis/importing_swift_into_objective-c
+        return self
+            .replacingOccurrences(of: "[^0-9a-zA-Z]", with: "_", options: .regularExpression)
+            .replacingOccurrences(of: "^[0-9]", with: "_", options: .regularExpression)
+    }
+}
